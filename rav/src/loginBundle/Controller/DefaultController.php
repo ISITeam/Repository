4a7a07f53b1e73c777341054a4da1a28c049077a
Yoan KECH <yoan.kech@gmail.com>
2015-07-02 16:12:03 +0200

    Revert "Ajout D'Inscription"
    This reverts commit e694e426d6418c7287387291aa6b3d35b55fb6ef.

--- rav/src/loginBundle/Controller/DefaultController.php
+++ rav/src/loginBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace loginBundle\Controller;
 
-use loginBundle\Entity\users;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -10,45 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/login")
+     * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction($name)
     {
-        $error = null;
-        $user = null;
-        var_dump($_POST);
-        if (isset($_POST["mail"])) {
-            $user = new users();
-            $user->setMail($_POST["mail"]);
-            $user->setPassword($_POST["password"]);
-            $user->setLogin($_POST["login"]);
-            $user->setDroit("membre");
-
-
-            //On fait ici appel au manager afin de pouvoir publier dans la BDD
-            try {
-                $entitymanager = $this->getDoctrine()->getManager();
-                $entitymanager->persist($user);
-                $entitymanager->flush();
-            } catch (\Exception $e) {
-                $error = "une erreur est survenue :" . $e->getMessage();
-            }
-        }
-        // On fait le passage de paramètres à la vue index.html.twig
-        return array('error' => $error, "indexAction" => $user);
-    }
-    public function allUsersAction()
-    {
-        $error = null;
-
-        $repository = $this->getDoctrine()
-            ->getRepository('loginBundle:User');
-        try {
-            $allProfile = $repository->findAll();
-        }catch(\Exception $e){
-            $error = "une erreur est survenue " . $e->getMessage();
-        }
-        return array('error' => $error, "allUser" => $allProfile);
+        return array('name' => $name);
     }
 }
